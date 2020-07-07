2e4a4fd3b8de7e0ca25a30f1c6bb6a5273117197
val8119 <mikk.vahaste@gmail.com>
2020-07-07 23:35:26 +0300

    Added style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,6 @@
 $(document).ready(function () {
-
     $(".dropdown-link").click(function () {
         $(".dropdown-container").slideUp('slow')
         $(this).closest('.dropdown').find('.dropdown-container').slideDown('slow');
-
-
     });
-
 });
