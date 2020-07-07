d2146f7475adc0bb22b5cf55e8b016fbb51fb61a
val8119 <mikk.vahaste@gmail.com>
2020-07-07 23:27:22 +0300

    Created *

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $(".dropdown-link").click(function () {
+        $(".dropdown-container").slideUp('slow')
+        $(this).closest('.dropdown').find('.dropdown-container').slideDown('slow');
+
+
+    });
+
+});
